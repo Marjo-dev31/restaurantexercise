55212516926e4829b19760ca585450b137a50140
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-08-16 16:29:38 +0200

    error page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const uuid = require('uuid');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,11 +28,21 @@ app.get('/restaurants', function (req, res) {
   });
 });
 
-app.get('/restaurants/:id', function (req,res) {
-const restaurantId = req.params.id;
-res.render('restaurant-details', { rid: restaurantId }); /*repond le fichier restaurant-details avec url /restaurants/restaurantId*/
+app.get('/restaurants/:id', function (req, res) {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
-})
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render('restaurant-details', { restaurant : restaurant }); /*premier restaurant = key du template , deuxieme = const du for*/
+    }
+  }
+
+  res.render('404'); /*repond page 404 si ne trouve pas le restaurant (en dehors de la boucle sinon ferait page 404 pour tous les restaurants qui ne sont pas de la meme id*/
+});
 
 app.get('/recommend', function (req, res) {
   res.render('recommend');
@@ -39,6 +50,7 @@ app.get('/recommend', function (req, res) {
 
 app.post('/recommend', function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4(); /*method qui ajoute à l objet restaurant une nouvelle propriete : un id unique*/
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
@@ -57,6 +69,14 @@ app.get('/confirm', function (req, res) {
 
 app.get('/about', function (req, res) {
   res.render('about');
+});
+
+app.use(function (req,res) {
+  res.render('404');
+}); /*middleware qui permet de repondre page 404 si l url n'existe pas*/
+
+app.use(function(error, req,  res, next) {
+  res.render('500');
 });
 
 app.listen(3000);
